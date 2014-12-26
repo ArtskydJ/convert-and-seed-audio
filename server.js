ed3ea62c157de21fb15fee77c54b34608edef597
Joseph Dykstra <josephdykstra@gmail.com>
2014-12-26 16:28:15 -0600

    maybe working still needs node-webkit still needs tests still needs to have something done with the tags, or delete the functionality entirely

--- server.js
+++ server.js
@@ -1,42 +1,59 @@
-var http = require('http')
 var crypto = require('crypto')
-var Socket = require('socket.io')
+var Server = require('socket.io')
 var WebTorrent = require('webtorrent')
 var sox = require('sox-stream')
 var xtend = require('xtend')
 var path = require('path')
-var Convertor = require('./convert.js')
+var each = require('async-each')
+var createTempFile = require('create-temp-file')
+//var Tags = require('./get-tag-data.js')
 
 //configuration
 var announce = [ "wss://tracker.webtorrent.io" ]
 var formats = ['mp3', 'ogg']
 
-config.ecstatic.root = process.cwd() + config.ecstatic.root
-var serve = Ecstatic(config.ecstatic)
-var server = http.createServer(serve)
-var io = Socket(server)
-var convert = Convertor()
+var io = new Server()
+//var tags = Tags()
 var torrenter = new WebTorrent()
 
 var upcomingSongs = [] //playing and upcoming
 server.listen(80)
 
-function onTorrent(torrent, cb) {
-	console.log('on torrent')
-	var file = torrent.files[0]
-	if (file) {
-		var stream = file.createReadStream()
-		var mp3Stream = convert('mp3', file.name, stream)
-		var oggStream = convert('ogg', file.name, stream)
+io.on('connect', function (socket) {
+	socket.on('upload', onUpload(function (err, hashes) {
+		if (!err) socket.emit('hashes', hashes)
+	}))
+})
 
-		var streams = formats.map(function (format) {
-			return convert(format, file.name, stream)
-		})
+function onUpload(done) {
+	return function ou(infHsh) {
+		torrenter.download(infHsh, function onTorrent(torrent) {
+			console.log('on torrent')
+			var file = torrent.files[0]
+			if (file) {
+				var stream = file.createReadStream()
+				each(formats, uploadFormat(file.name, stream), done)
+			} else {
+				done(new Error('No file found in torrent: ' + torrent && torrent.infoHash))
+			}
+		}
+	}
+}
 
-		//torrenter.seed(mp3)
-		//torrenter.seed(ogg)
-		//get both infohashes
-		//give em to the client
+function uploadFormat(filename, stream)
+	return function uf(format, next) {
+		var tmpFile = createTempFile()
+		var converted = convert(format, filename, stream)
+		if (converted) {
+			converted.pipe(tmpFile).on('finish', function () {
+				var newTorrent = torrenter.seed(tmpFile.path, function () {}) //skip the noop?
+				next(null, newTorrent.infoHash)
+			})
+		} else {
+			process.nextTick(function () {
+				next(torrent.infoHash)
+			})
+		}
 	}
 }
 
@@ -51,6 +68,3 @@ function isExtension(ext1, filename) {
 	var ext2 = path.extname(filename)
 	return ext1.toLowerCase() === ext2.toLowerCase()
 }
-
-//get rid of socket.io if possible
-//implement webtorrent
