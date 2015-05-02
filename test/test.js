9a5a5af9ecad50e504e25b06486065388a9b3970
Joseph Dykstra <josephdykstra@gmail.com>
2015-05-02 18:12:45 -0500

    removed mock webtorrent and cleaned up test.js

--- test/test.js
+++ test/test.js
@@ -1,52 +1,45 @@
-var MOCK = false
-
 var test = require('tape')
 var ServerInstance = require('../server/instance.js')
 var ClientInstance = require('../client/instance.js')
 var FileTransfer = require('../client/fileTransfer.js')
-var WebTorrent = require(MOCK ? './fixtures/mock-webtorrent.js' : 'webtorrent')
-
-function createTorrenters() {
-	var t1 = new WebTorrent()
-	var t2 = MOCK ? t1 : new WebTorrent()
-	return [ t1, t2 ]
-}
+var WebTorrent = require('webtorrent')
 
 function isString(x) {
 	return typeof x === 'string'
 }
 
 test('ogg file', function (t) {
-	var torrenters = createTorrenters()
-
-	var emitter = ServerInstance(torrenters[0])
-	var upload = ClientInstance(torrenters[1], emitter, isString)
-
-	var filename = __dirname + '/audio/test_1.ogg'
-	t.pass('Connecting, Seeding #1')
+	var torrenters = [ new WebTorrent(), new WebTorrent() ]
 	var timeStart = new Date().getTime()
 	var timeSeeding = null
+	var emitter = ServerInstance(torrenters[0])
+	var upload = ClientInstance(torrenters[1], emitter, isString)
+	var filename = __dirname + '/audio/test_1.ogg'
+
+	t.pass('Connecting, Seeding #1')
+
 	upload(filename, function onSeed(infhsh) {
 		timeSeeding = new Date().getTime()
 		var dur = (timeSeeding - timeStart) / 1000
 		t.pass('Seeding file #1, took ' + dur + ' seconds.')
+
 		originalInfoHash = infhsh
 		emitter.emit('upload', originalInfoHash)
 	})
 
 	emitter.on('error', function er(err) {
-		t.fail(err ? err.message : 'network error')
+		t.fail('network error' + (err && err.message))
 		end()
 	})
 
 	emitter.on('hashes', function ha(infoHashes) {
-		console.log('############################## hashes, get it?')
 		var timeHashes = new Date().getTime()
 		var dur = (timeHashes - timeSeeding) / 1000
 
 		t.equal(infoHashes.length, 2, '2 info hashes returned, took ' + dur + ' seconds.')
 
-		var msg = '"' + originalInfoHash + '" is in "' + infoHashes.join('", "') + '"'
+		var ih = infoHashes.map(function (s) { return s.slice(0, 10) }).join('", "')
+		var msg = '"' + originalInfoHash + '" is in "' + ih + '"'
 		t.notEqual(infoHashes.indexOf(originalInfoHash), -1, msg)
 		end()
 	})
@@ -64,39 +57,5 @@ test('ogg file', function (t) {
 		t.fail('timeout')
 		end()
 	}, 5 * 60 * 1000) //5 min
-
 	timeout.unref()
 })
-
-
-
-/*
-test('wav file', {timeout:60000}, function (t) {
-	var emitter = require('../')
-	var originalInfoHash = ''
-
-//on connect
-	webtorr.seed(__dirname + '/audio/test_2.wav', function (torrent) {
-		originalInfoHash = torrent.infoHash
-		emitter.emit('upload')
-	})
-
-	emitter.on('hashes', function (err, infoHashes) {
-		t.notOk(err, err? err.message : 'no error')
-		t.equal(infoHashes.length, 2, '2 info hashes returned')
-		t.equal(infoHashes.indexOf(originalInfoHash), -1, 'does not have original info hash')
-		end()
-	})
-
-	function end() {
-		t.end()
-		webtorr.destroy()
-		clearTimeout(timeoutId)
-	}
-
-	var timeoutId = setTimeout(function tmt() {
-		t.fail('timeout')
-		end()
-	}, 2 * 60 * 1000) //2 min
-})
-*/
