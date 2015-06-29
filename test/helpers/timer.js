02d62018639f12d2ecc831263c71bb0ef6b6895e
Joseph Dykstra <josephdykstra@gmail.com>
2015-06-29 07:52:05 -0500

    prettied timer in tests

--- test/helpers/timer.js
+++ test/helpers/timer.js
@@ -1,8 +1,12 @@
+function msToSec(ms) {
+	return Math.round(ms / 100) / 10
+}
+
 module.exports = function timer() {
 	var startTime = new Date().getTime()
 	return function timeDiff() {
 		var now = new Date().getTime()
-		var seconds = (now - startTime) / 1000
+		var seconds = msToSec(now - startTime)
 		startTime = now
 		return seconds
 	}
