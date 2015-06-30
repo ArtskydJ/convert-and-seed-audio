27ebcbe1b6440f70c2d9e0501b74d1a29090f05a
Joseph Dykstra <josephdykstra@gmail.com>
2015-06-29 22:06:15 -0500

    Added ability to connect to web peers, changed server api

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 var Instance = require('./instance.js')
-var WebTorrent = require('webtorrent')
 var shoe = require('shoe')
 var http = require('http')
 var emitStream = require('emit-stream')
 var JSONStream = require('JSONStream')
+try {
+	global.WRTC = require('wrtc')
+	global.WEBTORRENT_ANNOUNCE = [ 'wss://tracker.webtorrent.io' ]
+} catch (e) {
+	console.warn('Cannot connect to browser peers.', e.message)
+}
+var WebTorrent = require('webtorrent')
 
-module.exports = function createServer(reqListen) {
-	var torrenter = new WebTorrent()
-	var emitter = Instance(torrenter)
-	var sock = shoe(function (stream) {
-		emitStream(emitter)
-			.pipe(JSONStream.stringify())
-			.pipe(stream)
-	})
-	var server = http.createServer(reqListen)
-	sock.install(server, '/socket')
+module.exports = function casa(server) {
+	if (!server) server = http.createServer()
+	var emitter = Instance(new WebTorrent())
+	shoe(function (stream) {
+		emitStream(emitter).pipe(JSONStream.stringify()).pipe(stream)
+	}).install(server, '/socket')
 	return server
 }
